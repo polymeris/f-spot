ae931758540e7f73d8544d509c5612b88a1efd8b
Camilo Polymeris <cpolymeris@gmail.com>
2013-06-20 23:30:07 -0300

    Base class and simple implementation of the external developer handler

---
+++ src/Extensions/Tools/FSpot.Tools.OpenWithExternalDeveloper/FSpot.Tools.OpenWithExternalDeveloper/AbstractOpenWithExternalDeveloper.cs
@@ -0,0 +1,166 @@
+//  OpenWithExternalDeveloper.cs
+//
+//  Author:
+//       Camilo Polymeris <cpolymeris@gmail.com>
+//
+//  Copyright (c) 2013 Camilo Polymeris
+// 
+//  Contains code from: AbstractDevelopInUFRaw.cs
+//  Author:
+//   	Stephane Delcroix <stephane@delcroix.org>
+//   	Ruben Vermeersch <ruben@savanne.be>
+//
+//  Copyright (C) 2007-2010 Novell, Inc.
+//  Copyright (C) 2007-2009 Stephane Delcroix
+//  Copyright (C) 2008-2010 Ruben Vermeersch
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+using FSpot;
+
+using FSpot.Core;
+using FSpot.Extensions;
+using FSpot.Imaging;
+using Hyena;
+using Mono.Unix;
+
+namespace FSpot.Tools.OpenWithExternalDeveloper
+{
+	/// <summary>
+	/// Base class for external developer handlers. Uses the Development class to pass development parameters
+	/// to the derived classes' implamentation of CallExternalDeveloper.
+	/// Includes a file watcher to update the Photo with a new version when changes are registered.
+	/// </summary>
+	public abstract class AbstractOpenWithExternalDeveloper
+	{
+		/// Simple class to pass around and store development parameters
+		/// In the future might include JPEG parameters (e.g. quality)
+		protected class Development
+		{
+			public Development(Photo p)
+			{
+				photo = p;
+				if (!ImageFile.IsRaw (Raw.Uri))
+					throw new InvalidDataException();
+
+				version = GetVersionName (photo);
+				developed = GetUriForVersionName (photo, version);
+			}
+
+			public void StartWatching ()
+			{
+				watcher = new FileSystemWatcher ();
+				watcher.Path = Path.GetDirectoryName(developed.LocalPath); 
+				watcher.Filter = Path.GetFileName(developed.LocalPath);
+				watcher.Changed += new FileSystemEventHandler((sender, e) => this.OnDevelopedChanged());
+				watcher.Created += new FileSystemEventHandler((sender, e) => this.OnDevelopedChanged());
+				
+				watcher.EnableRaisingEvents = true;
+			}
+
+			public void StopWatching ()
+			{
+				watcher = null;
+			}
+
+			protected void OnDevelopedChanged ()
+			{
+				SafeUri uri = new SafeUri (developed);
+				
+				photo.AddVersion(uri.GetBaseUri(), uri.GetFilename(), version, true);
+				photo.Changes.DataChanged = true;
+				App.Instance.Database.Photos.Commit (photo);
+			}
+
+			public PhotoVersion Raw
+			{
+				get
+				{
+					return photo.GetVersion (Photo.OriginalVersionId) as PhotoVersion;
+				}
+			}
+
+			public System.Uri DevelopedUri
+			{
+				get {
+					return developed;
+				}
+			}
+
+			private Photo photo;
+			private string version; //< Name of the version of this photo the development is assigned to
+			private System.Uri developed; //< Path of output jpeg
+			private FileSystemWatcher watcher;
+		}
+
+		/// <summary>
+		/// Derived classes should reimplement this for program-specific functionality.
+		/// Remember to call d.StartWatching & d.StopWatching on program start and end, resp.
+		/// </summary>
+		protected abstract void CallExternalDeveloper (Development d);
+
+		public void Run (object o, EventArgs e, Photo p)
+		{
+			try
+			{
+				Development d = new Development(p);
+				CallExternalDeveloper (d);
+			}
+			catch (InvalidDataException)
+			{
+				Log.Warning ("The original version of this image is not a (supported) RAW file");
+				return;
+			}
+		}
+
+		protected static string GetVersionName (Photo p)
+		{
+			return GetVersionName (p, 1);
+		}
+		
+		private static string GetVersionName (Photo p, int i)
+		{
+			string name = Catalog.GetPluralString ("External development", "External development #{0}", i);
+			name = String.Format (name, i);
+			if (p.VersionNameExists (name))
+				return GetVersionName (p, i + 1);
+			return name;
+		}
+		
+		protected static System.Uri GetUriForVersionName (Photo p, string version_name)
+		{
+			string name_without_ext = System.IO.Path.GetFileNameWithoutExtension (p.Name);
+			return new System.Uri (System.IO.Path.Combine (DirectoryPath (p),  name_without_ext
+			                                               + " (" + version_name + ")" + ".jpg"));
+		}
+		
+		protected static string DirectoryPath (Photo p)
+		{
+			return p.VersionUri (Photo.OriginalVersionId).GetBaseUri ();
+		}
+	}	
+}
+
---
+++ src/Extensions/Tools/FSpot.Tools.OpenWithExternalDeveloper/FSpot.Tools.OpenWithExternalDeveloper/OpenWithExternalDeveloper.cs
@@ -0,0 +1,77 @@
+//  OpenWithExternalDeveloper.cs
+//
+//  Author:
+//       Camilo Polymeris <cpolymeris@gmail.com>
+//
+//  Copyright (c) 2013 Camilo Polymeris
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+using System;
+using FSpot.Extensions;
+
+namespace FSpot.Tools.OpenWithExternalDeveloper
+{
+	public class OpenWithExternalDeveloperCommand : ICommand
+	{
+		public const string PREFERENCES_EXTENSION =
+		Preferences.APP_FSPOT + "extension/openwithexternaldeveloper/";
+		public const string DEVELOPER_KEY = PREFERENCES_EXTENSION + "developer";
+
+		public void Run (object o, EventArgs e)
+		{
+			foreach (Photo p in App.Instance.Organizer.SelectedPhotos ())
+				try
+				{
+					GetHandler().Run(o, e, p);
+				}
+				catch (OperationCanceledException)
+				{
+					return;
+				}
+		}
+
+		protected AbstractOpenWithExternalDeveloper GetHandler()
+		{
+			if (handler == null)
+			switch (Preferences.Get<string>(DEVELOPER_KEY))
+			{
+				case "UFRaw":
+					handler = new OpenWithUFRaw ();
+				break;
+			}
+			return handler;
+		}
+
+		private AbstractOpenWithExternalDeveloper handler;
+    	}
+
+	public class ConfigureExternalDeveloperCommand : ICommand
+	{
+		public void Run (object o, EventArgs e)
+		{
+			dialog.Show ();
+		}
+
+		protected ExternalDeveloperConfigurationDialog dialog;
+	}
+}
+
